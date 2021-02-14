8373eb52826ffbd26b86b585b96f75b0bda5876b
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-02-14 17:08:32 +0900

    Update WeatherResponse.kt

--- app/src/main/java/kr/hs/dgsw/weathermap/data/model/response_model/WeatherResponse.kt
+++ app/src/main/java/kr/hs/dgsw/weathermap/data/model/response_model/WeatherResponse.kt
@@ -4,7 +4,7 @@ import kr.hs.dgsw.weathermap.data.model.entity.Main
 import kr.hs.dgsw.weathermap.data.model.entity.Weather
 import kr.hs.dgsw.weathermap.data.model.entity.Wind
 
-data class WeatherModel(
+data class WeatherResponse(
     val main: Main,
     val name: String,
     val weather: List<Weather>,
