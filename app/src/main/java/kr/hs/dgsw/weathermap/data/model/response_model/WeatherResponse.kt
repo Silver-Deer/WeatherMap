2d097eede89ccf82b3b400b742ac5b3be440f9e8
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-02-14 17:05:11 +0900

    [ADD] Response Model

---
+++ app/src/main/java/kr/hs/dgsw/weathermap/data/model/response_model/WeatherResponse.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.weathermap.data.model.response_model
+
+import kr.hs.dgsw.weathermap.data.model.entity.Main
+import kr.hs.dgsw.weathermap.data.model.entity.Weather
+import kr.hs.dgsw.weathermap.data.model.entity.Wind
+
+data class WeatherModel(
+    val main: Main,
+    val name: String,
+    val weather: List<Weather>,
+    val wind: Wind
+)
